779abec27c7812654110a5d1734a7541f4a0bf5f
Rasmus Kjellberg <rk@youngmedia.se>
2014-12-31 17:19:57 +0100

    0.0.1

---
+++ server.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Store connected users in a variable.
+var activeVisitors = 0;
+
+app.use(require('express').static('public'));
+
+app.get('/', function(req, res) {
+	res.sendFile( __dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+
+	activeVisitors++;
+	io.emit('activeVisitors', activeVisitors);
+	console.log('active visitors:', activeVisitors);
+
+	socket.on('disconnect', function(){
+		activeVisitors--;
+		io.emit('activeVisitors', activeVisitors);
+		console.log('active visitors:', activeVisitors);
+	});
+
+});
+
+http.listen(3000, function() {
+	console.log("Server started.");
+});
